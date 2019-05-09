53f3c30d87c527bbc26853ee2154c7e5e23c7968
rafikzoubli <47857724+rafikzoubli@users.noreply.github.com>
2019-05-09 11:36:07 +0100

    update

---
+++ rafik zoubli/score/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include"fonction.h"
+
+int main(){
+int collision=1;
+int i=0;
+i=gestion_score(collision,i);
+}
